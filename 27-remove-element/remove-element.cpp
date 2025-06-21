c251a82f13389da75d2832628880f76fdf210a6f
Md. Emran Hossen <92653363+Md-EmranHossen@users.noreply.github.com>
2025-06-21 19:29:33 +0600

    Time: 0 ms (100.00%) | Memory: 11.8 MB (5.20%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // Emran
+        vector <int> v;
+        for(auto num : nums){
+           if(num != val){
+            v.push_back(num);
+           }
+        }
+        nums.clear();
+        for(auto x : v){
+            nums.push_back(x);
+        }
+        return  v.size();
+    }
+};
